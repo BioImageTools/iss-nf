1998290b5d7e73e3f1f66ae0e6a6072a3dae8526
nv066607_gsk <nima.x.vakili@gsk.com>
2024-02-12 10:13:09 -0500

    small modification

---
+++ bin/spacetx.py
@@ -0,0 +1,17 @@
+import os
+import csv
+from slicedimage import ImageFormat
+from starfish.experiment.builder import format_structured_dataset
+         
+def spaceTx_format(input_path, csv, output_path):
+
+    format_structured_dataset(input_path, csv, output_path, ImageFormat.TIFF)
+
+    
+if __name__ == "__main__":
+    
+    spaceTx_format(input_dir, path_to_csv, output_dir)
+            
+            
+            
+            
