278bda05a2ac539ae97be4595fce77c7819c0d7a
nv066607_gsk <nima.x.vakili@gsk.com>
2024-02-14 08:04:20 -0500

    change the spotDetection format to works only by setting the tile number

--- bin/spot_detection.py
+++ bin/spot_detection.py
@@ -5,7 +5,7 @@ from starfish.types import Axes, FunctionSource
 
 
 def find_spots( json_path,
-                test_tile_idxs=None,
+                test_tile_idx,
                 images=None,
                 reference=None,
                 min_sigma=1,
@@ -15,37 +15,39 @@ def find_spots( json_path,
                 measurement_type='mean'
 )-> ImageStack:
     
+    final_spots = {}
     experiment = Experiment.from_json(json_path)
     
-    if test_tile_idxs is not None:
-        experiment = {fov_name: fov_data
-               for i, (fov_name, fov_data) in enumerate(experiment.items())
-               if i in test_tile_idxs}   
+    if test_tile_idx is not None:
+        exp = {fov_name: fov
+               for i, (fov_name, fov) in enumerate(experiment.items())
+               if i in test_tile_idx} 
+        
+    for fov_name, fov in exp.items():
+        primary = fov.get_image(FieldOfView.PRIMARY_IMAGES)
+        reference = fov.get_image('anchor_dots')
+        
+        if images is not None and reference is not None:
+            primary = images
+            reference = reference
 
-    fov = experiment.fov()
-    primary = fov.get_image(FieldOfView.PRIMARY_IMAGES)
-    reference = fov.get_image('anchor_dots')
-    
-    if images is not None and reference is not None:
-        primary = images
-        reference = reference
-    
-    bd = FindSpots.BlobDetector(
-        min_sigma=min_sigma,
-        max_sigma=max_sigma,
-        num_sigma=num_sigma,
-        threshold=threshold,
-        is_volume=False,
-        measurement_type=measurement_type,
-    )
-    dots_max = reference.reduce((Axes.ROUND, Axes.ZPLANE),
-                                      func='max')
-    spots = bd.run(image_stack=primary, reference_image=dots_max)
+        bd = FindSpots.BlobDetector(
+            min_sigma=min_sigma,
+            max_sigma=max_sigma,
+            num_sigma=num_sigma,
+            threshold=threshold,
+            is_volume=False,
+            measurement_type=measurement_type,
+        )
+        dots_max = reference.reduce((Axes.ROUND, Axes.ZPLANE),
+                                          func='max')
+        spots = bd.run(image_stack=primary, reference_image=dots_max)
 
-    return spots
-
+        final_spots[fov_name] = spots
+        
+    return final_spots
 
 if __name__ == "__main__":
     
     json_path = '/path/to/SpaceTx/primary/experiment.json'
-    find_spots(json_path)
+    find_spots(json_path, test_tile_idx=[9])
