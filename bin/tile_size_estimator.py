635884c8f496404384d54ea13fcca8f654e9d03a
nv066607_gsk <nima.x.vakili@gsk.com>
2024-02-19 11:26:54 -0500

    modify to cover smaller images

--- bin/tile_size_estimator.py
+++ bin/tile_size_estimator.py
@@ -1,32 +1,43 @@
 import tifffile as tif
 import fire
 
-def estimate_tile_size(image_path: str, max_size: int = 3000) -> None:
+def estimate_tile_size(image_path: str):
 
     img = tif.memmap(image_path)
     image_shape = img.shape
 
+    if min(image_shape) > 10000:
+        max_size = 3000
+        target_size = 2000
+        l1 = 1500
+        l2 = 3000
+    else:
+        max_size = 1000
+        target_size = 500
+        l1 = 150
+        l2 = 500
+        
     max_size = min(image_shape[0], image_shape[1], max_size)
-
     margin_size = {t: (image_shape[0] % t, image_shape[1] % t) for t in range(100, max_size, 100)}
-
     pairs = []
     for s, (r, c) in margin_size.items():
-        if 1500 < r < 3000 and 1500 < c < 3000:
+        if l1 < r < l2 and l1 < c < l2:
             pairs.append([s, [r, c]])
-
     min_difference = float('inf')
     selected_pair = None
 
     for pair in pairs:
         difference = abs(pair[1][0] - pair[1][1])
-
         if difference < min_difference or (difference == min_difference and selected_pair is not None):
             min_difference = difference
             selected_pair = pair[1]
             selected_tileSize = pair[0]
 
-    print("Selected Tile Size:", selected_tileSize)
+    if min(image_shape) > 10000:
+        selected_tileSize = min(pairs, key=lambda x: abs(x[0] - target_size))[0]
+    else:
+        selected_tileSize = min(pairs, key=lambda x: abs(x[0] - target_size))[0]
+    return selected_tileSize
 
 if __name__ == "__main__":
     cli = {
