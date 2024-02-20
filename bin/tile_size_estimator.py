9e01753cc9740c85cfa3489411587df3d0fed28a
nv066607_gsk <nima.x.vakili@gsk.com>
2024-02-20 04:22:07 -0500

    add plot and new boundaries for small images

--- bin/tile_size_estimator.py
+++ bin/tile_size_estimator.py
@@ -1,24 +1,41 @@
 import tifffile as tif
 import fire
 
-def estimate_tile_size(image_path: str):
+
+def plot_tile_grid(image, tileSize):
+    plt.imshow(image, cmap='gray')
+    height, width = image.shape[:2]
+    
+    for i in range(0, width, tileSize):
+        plt.axvline(i, color='red')
+        
+    for j in range(0, height, tileSize):
+        plt.axhline(j, color='red')
+    
+    plt.ylim(height, 0) 
+    plt.gca().set_aspect('equal', adjustable='box') 
+    plt.show()
+    
+def estimate_tile_size(image_path: str, verbose=False):
 
     img = tif.memmap(image_path)
     image_shape = img.shape
-
+    
     if min(image_shape) > 10000:
         max_size = 3000
         target_size = 2000
         l1 = 1500
-        l2 = 3000
+        l2 = 3000 
+        max_size = min(image_shape[0], image_shape[1], max_size)
+        margin_size = {t: (image_shape[0] % t, image_shape[1] % t) for t in range(100, max_size, 100)}
     else:
-        max_size = 1000
-        target_size = 500
-        l1 = 150
-        l2 = 500
+        max_size = int(min(image_shape)/2)
+        target_size = int(min(image_shape)/4)
+        l1 = int(min(image_shape)/10)
+        l2 = int(min(image_shape)/2)
+        max_size = min(image_shape[0], image_shape[1], max_size)
+        margin_size = {t: (image_shape[0] % t, image_shape[1] % t) for t in range(10, max_size, 10)}
         
-    max_size = min(image_shape[0], image_shape[1], max_size)
-    margin_size = {t: (image_shape[0] % t, image_shape[1] % t) for t in range(100, max_size, 100)}
     pairs = []
     for s, (r, c) in margin_size.items():
         if l1 < r < l2 and l1 < c < l2:
@@ -37,6 +54,10 @@ def estimate_tile_size(image_path: str):
         selected_tileSize = min(pairs, key=lambda x: abs(x[0] - target_size))[0]
     else:
         selected_tileSize = min(pairs, key=lambda x: abs(x[0] - target_size))[0]
+        
+    if verbose:
+        plot_tile_grid(img, selected_tileSize)
+
     return selected_tileSize
 
 if __name__ == "__main__":
