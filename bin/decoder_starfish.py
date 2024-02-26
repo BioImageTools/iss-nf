7e3144c8205d7cc540317e64971dae4f47edab9d
Sebastian Gonzalez-Tirado <sebastian.gonzalez@embl.de>
2024-02-26 13:50:05 +0100

    Add decoding script to process single FoV

--- bin/decoder_starfish.py
+++ bin/decoder_starfish.py
@@ -1,10 +1,63 @@
+import fire
+import pickle
+import os
+import numpy as np
 from starfish.spots import DecodeSpots
+from starfish.core.imagestack.imagestack import ImageStack
+from starfish.types import Axes, FunctionSource
+from starfish.spots import FindSpots, DecodeSpots, AssignTargets
 from starfish.core.types import SpotFindingResults
 from starfish.core.intensity_table.decoded_intensity_table import DecodedIntensityTable
-from starfish import Experiment
+from starfish import Experiment, FieldOfView
+from starfish.image import LearnTransform, ApplyTransform, Filter, Segment
+from starfish.core.spots.DecodeSpots.trace_builders import build_spot_traces_exact_match
 
+#### Auxiliary functions:
+def register(
+    image_stack: ImageStack,
+    reference_stack: ImageStack,
+) -> ImageStack:
+    learn_translation = LearnTransform.Translation(
+        reference_stack=reference_stack, axes=Axes.ROUND, upsampling=100)
 
+    transforms_list = learn_translation.run(
+        image_stack.reduce({Axes.CH, Axes.ZPLANE}, func='max'))
 
+    warp = ApplyTransform.Warp()
+    registered = warp.run(image_stack, transforms_list=transforms_list,
+                            in_place=False, verbose=False)
+
+    return registered
+
+def find_spots(
+    image_stack: ImageStack, 
+    reference_stack: ImageStack
+) -> SpotFindingResults:
+    """Detect spots using laplacian of gaussians approach."""
+    bd = FindSpots.BlobDetector(
+                min_sigma=1,
+                max_sigma=2,
+                num_sigma=30,
+                threshold=0.08,
+                is_volume=False,
+                measurement_type='mean')
+    dots_max = reference_stack.reduce((Axes.ROUND, Axes.ZPLANE),
+                                       func='max')
+    # Locate spots in reference image
+    spots = bd.run(image_stack=image_stack, reference_image=dots_max)
+
+    return spots
+
+def decode(spots: SpotFindingResults, experiment) -> DecodedIntensityTable:
+    """Decode pixel traces using the codebook."""
+    decoder = DecodeSpots.PerRoundMaxChannel(
+        codebook=experiment.codebook,
+    )
+    decoded = decoder.run(spots=spots)
+
+    return decoded
+
+"""
 def decode_starfish(spots: SpotFindingResults, json_path) -> DecodedIntensityTable:
 
     exp = Experiment.from_json(json_path)
@@ -18,58 +71,62 @@ def decode_starfish(spots: SpotFindingResults, json_path) -> DecodedIntensityTab
         decoded_spots[fov_name] = decoded
 
     return decoded_spots
+"""
 
-def _process_fov(
-    fov: FieldOfView,
-    fov_name,
-    normalize: bool = False,
-    register: bool = True,
-    filt: bool = True,
-    reg_withDapi=True,
+def process_fov(
+    images_dir_path,
+    fov_name: str
 ):
-        primary = fov.get_image(FieldOfView.PRIMARY_IMAGES)
-        reference = fov.get_image('anchor_dots')
-        dapi_rounds = fov.get_image('nuclei')
-        dapi_ref = fov.get_image('anchor_nuclei')
+    exp = Experiment.from_json(os.path.join(images_dir_path, 'experiment.json'))
+    fov = exp[fov_name]
+    primary = fov.get_image(FieldOfView.PRIMARY_IMAGES)
+    reference = fov.get_image('anchor_dots')
+    dapi_rounds = fov.get_image('nuclei')
+    dapi_ref = fov.get_image('anchor_nuclei')
 
-        if normalize:
-            primary = self._normalize(image_stack=primary)
+    #if normalize:
+    #    primary = _normalize(image_stack=primary)
             
-        if register:
-            if reg_withDapi:
-                primary_registered = self._register_new(
-                                         fov_name,
-                                         image_stack=primary,
-                                         dapi_ref=dapi_ref,
-                                         dapi_round=dapi_rounds
-                                         )
-            else:
-                primary_registered = self._register(
-                                         fov_name,
-                                         reference_stack=reference,
-                                         image_stack=primary,
-                                         )
+    #if True:
+        #if reg_withDapi:
+        #primary_registered = self._register_new(
+        #                                 fov_name,
+        #                                 image_stack=primary,
+        #                                 dapi_ref=dapi_ref,
+        #                                 dapi_round=dapi_rounds
+        #                                 )
+        #    else:
+    #primary_registered = register(
+    #                        reference_stack=reference,
+    #                        image_stack=primary
+    #                        )
 
-        if filt:
-            filtered_imgs, filtered_ref = self._filter(
-                                    reference_stack=reference,
-                                    image_stack=primary)
+    filtered_ref = reference
+    filtered_imgs = primary#_registered
 
+    spots = find_spots(image_stack=filtered_imgs,
+                       reference_stack=filtered_ref)
 
-        if filt is False:
-            filtered_ref = reference
-            filtered_imgs = primary_registered
+    #decoded = decode(spots, exp)
+    
+    spots4postcode = build_spot_traces_exact_match(spots)
+    
+    np.save('spot_traces.npy', spots4postcode)
+    # Serialize the object
+    #with open('data.pickle', 'wb') as file: 
+    #    pickle.dump(spots, file)
 
-        spots = self._find_spots(image_stack=filtered_imgs,
-                                 reference_stack=filtered_ref)
-
-        decoded = self._decode(spots)
-
-        return decoded, spots, fov_name
+    #return spots#decoded, spots, fov_name
+    
+def read_fov(
+    spots_pickle
+):
+    with open(spots_pickle, 'rb') as f:
+        fov_results = pickle.load(f)
 
 if __name__ == "__main__":
-    
-    spots = '/from/spotDetection/only/for/one/tile'
-    json_path = '/hpc/scratch/hdd1/nv066607/ISS_tmp/ISS_471_NSCLC253_BL2/SpaceTx/primary/experiment.json'
-    data_to_decode = next(iter(spots.values()))
-    decode_starfish(data_to_decode, json_path)
+    cli = {
+        "decode_fov": process_fov,
+        "read_fov": read_fov
+    }
+    fire.Fire(cli)
